9e51ce3c998e278f68c8ed09569953b9b6ca585b
sarahmocknick <142832943+sarahmocknick@users.noreply.github.com>
2023-11-01 18:08:18 -0400

    Sent unemployment email (#2)

--- app/email_service.py
+++ app/email_service.py
@@ -15,49 +15,50 @@ SENDER_ADDRESS = os.getenv("SENDER_ADDRESS")
 
 
 def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Testing 123", html_content="<p>Hello World</p>"):
-    print("SENDING EMAIL TO:", recipient_address)
-    print("SUBJECT:", subject)
-    print("HTML:", html_content)
+    if __name__ == "__main__":
+        print("SENDING EMAIL TO:", recipient_address)
+        print("SUBJECT:", subject)
+        print("HTML:", html_content)
 
-    client = SendGridAPIClient(SENDGRID_API_KEY)
-    print("CLIENT:", type(client))
+        client = SendGridAPIClient(SENDGRID_API_KEY)
+        print("CLIENT:", type(client))
 
-    message = Mail(from_email=SENDER_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html_content)
+        message = Mail(from_email=SENDER_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html_content)
 
-    try:
-        response = client.send(message)
+        try:
+            response = client.send(message)
 
-        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-        print(response.status_code) #> 202 indicates SUCCESS
-        print(response.body)
-        print(response.headers)
+            print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+            print(response.status_code) #> 202 indicates SUCCESS
+            print(response.body)
+            print(response.headers)
 
-    except Exception as err:
-        print(type(err))
-        print(err)
-
-
-
-
-user_address = input("Please enter your email address: ")
+        except Exception as err:
+            print(type(err))
+            print(err)
 
 
-my_content = """
 
-    <img
-        src="https://img.freepik.com/free-vector/flat-ice-cream-collection_23-2148982427.jpg"
-        alt="image of an ice cream"
-        height=100
-    >
 
-    <h1>Ice Cream Shop Menu</h1>
+    user_address = input("Please enter your email address: ")
 
-    <p>Most popular flavors:</p>
 
-    <ul>
-        <li>Vanilla Bean </li>
-        <li>Choc </li>
-        <li>Strawberry</li>
-    </ul>
-"""
-send_email(html_content=my_content, recipient_address=user_address)
+    my_content = """
+
+        <img
+            src="https://img.freepik.com/free-vector/flat-ice-cream-collection_23-2148982427.jpg"
+            alt="image of an ice cream"
+            height=100
+        >
+
+        <h1>Ice Cream Shop Menu</h1>
+
+        <p>Most popular flavors:</p>
+
+        <ul>
+            <li>Vanilla Bean </li>
+            <li>Choc </li>
+            <li>Strawberry</li>
+        </ul>
+    """
+    send_email(html_content=my_content, recipient_address=user_address)
--- app/unemployment.py
+++ app/unemployment.py
@@ -7,10 +7,12 @@ import os
 import json
 from pprint import pprint
 from statistics import mean
-#Packages
+#Packages that do need installation via pip
 from dotenv import load_dotenv
 import requests
 from plotly.express import line
+
+from app.email_service import send_email
 
 #Environment variables and constants
 load_dotenv()
@@ -76,3 +78,18 @@ rates = [float(d["value"]) for d in data]
 
 fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
 fig.show()
+
+user_address = input("Please enter your email address: ")
+
+
+
+latest_rate = data[0]['value']
+latest_date = data[0]["date"]
+
+content = f"""
+<h1> Unemployment Report Email </h1>
+
+<p> Latest rate: {latest_rate}% as of {latest_date} </p>
+"""
+
+send_email(recipient_address=user_address, html_content=content, subject="Your Unemployment Report")
